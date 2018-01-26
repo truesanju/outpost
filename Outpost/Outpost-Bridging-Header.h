b8fddc7189c4064a083ebfa2d4fac502ad8be397
Sanjay <sanjaypushparajan@gmail.com>
2018-01-26 13:21:57 +0800

    Initial commit

---
+++ Outpost/Outpost-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Outpost-Bridging-Header.h
+//  Outpost
+//
+//  Created by Sanjay Pushparajan on 1/14/18.
+//  Copyright © 2018 Sanjay Pushparajan. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
---
+++ Outpost/OutpostConnector.swift
@@ -0,0 +1,16 @@
+//
+//  OutpostConnector.swift
+//  Outpost
+//
+//  Created by Sanjay Pushparajan on 1/23/18.
+//  Copyright © 2018 Sanjay Pushparajan. All rights reserved.
+//
+
+import Foundation
+import ARKit
+import WebKit
+
+class OutpostConnector: NSObject, ARSessionDelegate {
+    
+    
+}
