6ded74dc01b685acd5e64a0bed73363fdbfeb2a3
Sanjay <sanjaypushparajan@gmail.com>
2018-02-14 23:43:32 +0800

    connected to three.js

--- Outpost/OutpostConnector.swift
+++ Outpost/OutpostConnector.swift
@@ -10,7 +10,46 @@ import Foundation
 import ARKit
 import WebKit
 
-class OutpostConnector: NSObject, ARSessionDelegate {
+class OutpostConnector: NSObject, ARSessionDelegate, WKUIDelegate {
     
+    var webView: WKWebView!
+    var session: ARSession!
+    
+    let DEFAULT_URL = "www.google.com"
+    
+    func drawWebView (onParentView:UIView, usingARSession: ARSession) {
+        self.session = usingARSession
+        
+        //Set up webview size
+        let viewsizeRect = onParentView.bounds
+        let webConfiguration = WKWebViewConfiguration()
+        self.webView = WKWebView(frame: viewsizeRect, configuration: webConfiguration)
+        loadURL(URLAsString: DEFAULT_URL)
+        onParentView.addSubview(webView)
+    }
+    
+    func loadURL (URLAsString: String){
+        let URLToLoad = URL(fileURLWithPath: URLAsString)
+        let URLLoadRequest = URLRequest(url: URLToLoad)
+        self.webView.load(URLLoadRequest)
+    }
+    
+    //Store URL in user defaults
+    func storeURLInDefaults (urlToStore:String){
+        let defaults = UserDefaults.standard
+        defaults.set(urlToStore, forKey: "url")
+    }
+    
+    
+    //Fetch current default URL
+    func getURLFromDefaults () -> String? {
+        let defaults = UserDefaults.standard
+        if let url = defaults.string(forKey: "url") {
+            return url
+        } else {
+            print("No default URL found.")
+            return nil
+        }
+    }
     
 }
--- Outpost/ViewController.swift
+++ Outpost/ViewController.swift
@@ -12,11 +12,18 @@ import MetalKit
 import ARKit
 import WebKit
 import SpriteKit
+import Foundation
 
 extension MTKView : RenderDestinationProvider {
 }
 
 class ViewController: UIViewController, MTKViewDelegate, ARSessionDelegate {
+    
+    let DEBUG = true
+    let DEFAULT_URL = "https://outpost-web.herokuapp.com/"
+    let DEV_URL = "https://8d75ae0e.ngrok.io/"
+    
+    var webView:WKWebView!
     
     var session: ARSession!
     var renderer: Renderer!
@@ -30,8 +37,11 @@ class ViewController: UIViewController, MTKViewDelegate, ARSessionDelegate {
         session = ARSession()
         session.delegate = self
         
+       
+        
         // Set the view to use the default device
         if let view = self.view as? MTKView {
+            
             view.device = MTLCreateSystemDefaultDevice()
             view.backgroundColor = UIColor.clear
             view.delegate = self
@@ -41,24 +51,43 @@ class ViewController: UIViewController, MTKViewDelegate, ARSessionDelegate {
                 return
             }
             
+            //Create web view
+            let userContentController = WKUserContentController()
+            let webConfiguration = WKWebViewConfiguration()
+            webConfiguration.userContentController = userContentController
+            
+            webView = WKWebView(frame: view.bounds, configuration: webConfiguration)
+            webView.isOpaque = false
+            webView.backgroundColor = UIColor.clear
+            webView.scrollView.backgroundColor = UIColor.clear
+            webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            
+            //Add as subview to MTKView
+            view.addSubview(webView)
+            self.loadURL(URLAsString: DEV_URL)
+            
             // Configure the renderer to draw to the view
             renderer = Renderer(session: session, metalDevice: view.device!, renderDestination: view)
             
             renderer.drawRectResized(size: view.bounds.size)
+            
+            
         }
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap(gestureRecognize:)))
         view.addGestureRecognizer(tapGesture)
         
-        //Clear website cache
-        let allWebsiteDataTypes = WKWebsiteDataStore.allWebsiteDataTypes()
-        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: allWebsiteDataTypes) { (data) in
-            WKWebsiteDataStore.default().removeData(ofTypes: allWebsiteDataTypes, for: data, completionHandler: {
-                print("Website cache cleared successfully.")
-            })
-        }
+//        //Clear website cache
+//        let allWebsiteDataTypes = WKWebsiteDataStore.allWebsiteDataTypes()
+//        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: allWebsiteDataTypes) { (data) in
+//            WKWebsiteDataStore.default().removeData(ofTypes: allWebsiteDataTypes, for: data, completionHandler: {
+//                print("Website cache cleared successfully.")
+//            })
+//        }
         
         //TODO: Load WebXR polyfill
+        
+      
         
         
     }
@@ -92,7 +121,7 @@ class ViewController: UIViewController, MTKViewDelegate, ARSessionDelegate {
             
             // Create a transform with a translation of 0.2 meters in front of the camera
             var translation = matrix_identity_float4x4
-            translation.columns.3.z = -2
+            translation.columns.3.z = -0.2
             let transform = simd_mul(currentFrame.camera.transform, translation)
             
             // Add a new anchor to the session
@@ -102,6 +131,11 @@ class ViewController: UIViewController, MTKViewDelegate, ARSessionDelegate {
     }
     
     // MARK: - MTKViewDelegate
+    func loadURL (URLAsString: String){
+        let URLToLoad = URL(string: URLAsString)
+        let URLLoadRequest = URLRequest(url: URLToLoad!)
+        webView.load(URLLoadRequest)
+    }
     
     // Called whenever view changes orientation or layout is changed
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
@@ -129,4 +163,66 @@ class ViewController: UIViewController, MTKViewDelegate, ARSessionDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    
+    //Called whenever frame updates
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        DispatchQueue.main.async {
+            
+            let orientation = UIApplication.shared.statusBarOrientation
+            let viewMatrix = frame.camera.viewMatrix(for: orientation)
+            let projectionMatrix = frame.camera.projectionMatrix(for: orientation, viewportSize: self.webView.frame.size, zNear: 0.02, zFar: 20)
+            
+            let viewMatrixAsArray = self.Float4x4ToArray(matrix: simd_inverse(viewMatrix))
+            let projectionMatrixAsArray = self.Float4x4ToArray(matrix: projectionMatrix)
+            
+            let serializedViewMatrix = self.serializeMatrix(matrixToSerialize:(viewMatrix))
+            let serializedProjectionMatrix = self.serializeMatrix(matrixToSerialize: projectionMatrix)
+            
+            let lightEstimate = frame.lightEstimate
+            
+            if(lightEstimate != nil){
+                let ambient:Float = Float(lightEstimate!.ambientIntensity/1000.0)
+                //let temperature = lightEstimate!.ambientColorTemperature
+                
+                self.webView.evaluateJavaScript("ambientLightUpdate('\(ambient)');", completionHandler: { (result, error) in
+                    print("Light estimate updated")
+                });
+            }
+            
+           
+            
+            self.webView.evaluateJavaScript("cameraMatrixUpdate('\(viewMatrixAsArray)','\(projectionMatrixAsArray)');", completionHandler: { (result, error) in
+                
+                print(result)
+                
+                if(error != nil){
+                    print(error)
+                }
+            })
+
+        }
+    }
+    
+    func Float3ToArray(matrix: simd_float3) -> [Float] {
+        return [matrix.x,matrix.y,matrix.z]
+    }
+    
+    func Float4x4ToArray(matrix: simd_float4x4) -> [Float] {
+        return [matrix.columns.0.x, matrix.columns.0.y, matrix.columns.0.z, matrix.columns.0.w,
+                matrix.columns.1.x, matrix.columns.1.y, matrix.columns.1.z, matrix.columns.1.w,
+                matrix.columns.2.x, matrix.columns.2.y, matrix.columns.2.z, matrix.columns.2.w,
+                matrix.columns.3.x, matrix.columns.3.y, matrix.columns.3.z, matrix.columns.3.w]
+    }
+    
+    func serializeMatrix (matrixToSerialize:matrix_float4x4) -> String? {
+        let encoder = JSONEncoder()
+        do{
+            let matrixJSONData = try encoder.encode(Float4x4ToArray(matrix: matrixToSerialize))
+            return String(data: matrixJSONData, encoding: String.Encoding.utf8)!
+        } catch {
+            print("Unable to serialize matrix")
+            return nil
+        }
+    }
+    
 }
